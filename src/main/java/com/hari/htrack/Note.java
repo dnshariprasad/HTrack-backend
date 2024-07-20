e8b4acdb4e52e339eb0e365fff9cb13ad99a8767
Hari <dnshariprasad@gmail.com>
2024-07-20 23:53:19 +0530

    simple CRUD

--- src/main/java/com/hari/htrack/Note.java
+++ src/main/java/com/hari/htrack/Note.java
@@ -1,34 +1,82 @@
 package com.hari.htrack;
 
 public class Note {
-    private long id;
+    private int id;
     private String title;
     private String info;
+    private String link;
+    private String type;
+    private String tags;
 
-    public Note(long id, String title, String info) {
+    public Note(int id, String title, String info, String link, String type, String tags) {
         this.id = id;
         this.title = title;
         this.info = info;
+        this.link = link;
+        this.type = type;
+        this.tags = tags;
     }
 
-    public long getId() {
+    public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getInfo() {
         return info;
     }
 
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Note && id == ((Note) o).id;
+    }
+
     @Override
     public String toString() {
-        return "Note{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", info='" + info + '\'' +
-                '}';
+        return super.toString();
     }
 }
--- src/main/java/com/hari/htrack/NotesController.java
+++ src/main/java/com/hari/htrack/NotesController.java
@@ -1,18 +1,70 @@
 package com.hari.htrack;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
+@RequestMapping("/notes")
 public class NotesController {
-    @RequestMapping("/notes")
+    private List<Note> notes = new ArrayList<>();
+
+    public NotesController() {
+        notes.add(new Note(1, "orci, adipiscing non, luctus sit amet, faucibus ut, nulla.", "feugiat. Sed nec metus facilisis lorem", "http://naver.com", "nec orci. Donec nibh.", "enim. Nunc ut erat. Sed"));
+        notes.add(new Note(2, "tempor arcu. Vestibulum ut eros", "dui nec urna suscipit", "http://walmart.com", "elit, pharetra ut, pharetra sed, hendrerit a, arcu. Sed", "luctus lobortis. Class aptent taciti sociosqu ad litora torquent"));
+        notes.add(new Note(3, "ligula. Nullam feugiat", "sapien. Nunc pulvinar arcu et pede. Nunc sed orci", "http://google.com", "lectus quis", "lorem lorem, luctus ut, pellentesque eget, dictum"));
+        notes.add(new Note(4, "vulputate, posuere vulputate, lacus. Cras", "ipsum nunc id enim. Curabitur massa.", "http://youtube.com", "faucibus lectus, a sollicitudin orci sem", "libero lacus, varius"));
+        notes.add(new Note(5, "urna, nec luctus felis purus ac tellus. Suspendisse sed", "commodo hendrerit. Donec porttitor", "http://cnn.com", "consectetuer mauris id", "eu neque pellentesque massa lobortis"));
+    }
+
+    @GetMapping
     public List<Note> notesList() {
-        return Arrays.asList(
-                new Note(1, "a", "aaa"),
-                new Note(1, "b", "bbb")
-        );
+        return notes.stream().toList();
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> createItem(@RequestBody Note note) {
+        notes.add(note);
+        return ResponseEntity.ok(note);
+    }
+
+    @PutMapping
+    public ResponseEntity<Note> updateNote(@RequestBody Note note) {
+        Note n = null;
+        for (Note cn : notes) {
+            if (note.getId() == cn.getId()) {
+                n = cn;
+                break;
+            }
+        }
+        if (null != n) {
+            n.setId(note.getId());
+            n.setInfo(note.getInfo());
+            n.setLink(note.getLink());
+            n.setTags(note.getTags());
+            n.setTitle(note.getTitle());
+            n.setType(note.getType());
+            return ResponseEntity.ok(note);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Note> deleteNote(@PathVariable int id) {
+        Note n = null;
+        for (Note cn : notes) {
+            if (id == cn.getId()) {
+                n = cn;
+                break;
+            }
+        }
+        if (null != n) {
+            notes.remove(n);
+            return ResponseEntity.ok(n);
+        } else
+            return ResponseEntity.notFound().build();
     }
 }
